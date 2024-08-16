35ce8585d659e3681fa68bd4973201d67e394cd0
ammaarkhan <ammaarkhan03@gmail.com>
2024-08-16 12:21:17 +0530

    minor edits

--- brokol-website/src/app/page.js
+++ brokol-website/src/app/page.js
@@ -26,7 +26,7 @@ async function handleEmailSubmission(email) {
   try {
     await addDoc(collection(db, "emails"), {
       email: email,
-      timestamp: new Date().toISOString(),
+      timeStamp: new Date(),
     });
   } catch (e) {
     console.error("Error adding document: ", e);
@@ -300,7 +300,7 @@ export default function Home() {
         <section id="testimonials" className="bg-white text-black py-16">
           <div className="container mx-auto text-left max-w-4xl">
             <h2 className="text-3xl font-normal mb-8 px-5">
-              Don’t just take our words.{" "}
+              Don't just take our words.{" "}
               <span className="font-bold">
                 Read what people with ADHD say about our meal planner.
               </span>
